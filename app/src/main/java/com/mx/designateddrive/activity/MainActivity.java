8aeb27ed2be29d79c5425cc0dde29c93f3b34263
jun.min.gyun@gmail.com <jun.min.gyun@gmail.com>
2021-01-04 18:49:22 +0900

    aa

--- app/src/main/java/com/mx/designateddrive/activity/MainActivity.java
+++ app/src/main/java/com/mx/designateddrive/activity/MainActivity.java
@@ -1,12 +1,19 @@
 package com.mx.designateddrive.activity;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mx.designateddrive.R;
 
@@ -16,6 +23,8 @@ import static com.mx.designateddrive.Constants.START_POINT;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView searchStartPointText, searchEndPointText;
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         findViewById(R.id.search_start_point_layout).setOnClickListener(this);
         findViewById(R.id.search_end_point_layout).setOnClickListener(this);
+
+        // permission check
+        if (ContextCompat.checkSelfPermission(this, REQUIRED_PERMISSIONS[0])
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(REQUIRED_PERMISSIONS, 0);
+            }
+        }else {
+
+        }
     }
 
     @Override
@@ -52,21 +72,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        String location = data.getStringExtra("location");
+        String location = null;
+        if (data != null) {
+            location = data.getStringExtra("location");
+        }
 
-        if(requestCode == START_POINT){
-            if (resultCode==RESULT_OK) {
+        if (requestCode == START_POINT) {
+            if (resultCode == RESULT_OK) {
                 searchStartPointText.setText(location);
-            }else{
+            } else {
 
             }
-        }else if(requestCode == END_POINT){
-            if (resultCode==RESULT_OK) {
+        } else if (requestCode == END_POINT) {
+            if (resultCode == RESULT_OK) {
                 searchEndPointText.setText(location);
-            }else{
+            } else {
 
             }
         }
 
+    }
+
+    // ActivityCompat.requestPermissions를 사용한 퍼미션 요청의 결과를 리턴받는 메소드
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grandResults) {
+
+        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+
+            // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
+            boolean check_result = true;
+
+            // 모든 퍼미션을 허용했는지 체크합니다.
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+
+            if (check_result) {
+                Log.d("@@@", "start");
+                //위치 값을 가져올 수 있음
+
+            } else {
+                // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있다
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
+                    Toast.makeText(this, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    Toast.makeText(this, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/mx/designateddrive/activity/MapSearchActivity.java
+++ app/src/main/java/com/mx/designateddrive/activity/MapSearchActivity.java
@@ -1,24 +1,40 @@
 package com.mx.designateddrive.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
 
 import com.mx.designateddrive.R;
 
 import static com.mx.designateddrive.Constants.END_POINT;
 import static com.mx.designateddrive.Constants.START_POINT;
 
-public class MapSearchActivity extends AppCompatActivity implements View.OnClickListener{
+public class MapSearchActivity extends AppCompatActivity implements View.OnClickListener, MapView.MapViewEventListener, MapView.CurrentLocationEventListener {
 
     int whatSearch;
-    Button submitBtn;
+    Button submitBtn, address_search_btn;
     TextView address_name, address;
+    EditText address_search_text;
+    ViewGroup mapViewContainer;
+    MapView mapView;
+    double currentLatitude, currentLongitude;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,14 +44,17 @@ public class MapSearchActivity extends AppCompatActivity implements View.OnClick
         submitBtn = findViewById(R.id.submit_btn);
         address_name = findViewById(R.id.address_name);
         address = findViewById(R.id.address);
+        mapViewContainer = findViewById(R.id.map_view);
+        address_search_text = findViewById(R.id.address_search_text);
 
         findViewById(R.id.submit_btn).setOnClickListener(this);
+        findViewById(R.id.address_search_btn).setOnClickListener(this);
 
         /* 검색할 데이터에 따라 view setting */
         whatSearch = getIntent().getIntExtra("search", START_POINT);
         setView();
 
-
+        createMapView();
     }
 
     @Override
@@ -46,6 +65,14 @@ public class MapSearchActivity extends AppCompatActivity implements View.OnClick
                 intent.putExtra("location", address.getText());
                 setResult(RESULT_OK, intent);
                 finish();
+                break;
+            case R.id.address_search_text:
+                break;
+            case R.id.address_search_btn:
+                String url = "kakaomap://search?q="+address_search_text.getText()+"&p="+currentLatitude+","+currentLongitude;
+                Log.e("url", url);
+                Intent intent2 = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent2);
                 break;
             default:
                 Log.e("MapSearchActivity", "no click event");
@@ -60,8 +87,108 @@ public class MapSearchActivity extends AppCompatActivity implements View.OnClick
             case END_POINT:
                 submitBtn.setText("도착지로 설정");
                 break;
-
+            default:
+                Log.e("MapSearchAcitiv setView", "fail");
         }
 
     }
+
+    private void createMapView() {
+        mapView = new MapView(this);
+        MapPOIItem marker = new MapPOIItem();
+
+        mapView.setCurrentLocationEventListener(this);
+        mapView.setMapViewEventListener(this);
+
+//        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(currentLatitude, currentLongitude), true);
+        mapView.zoomIn(true);
+        mapView.zoomOut(true);
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
+
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MapPoint.mapPointWithGeoCoord(currentLatitude, currentLongitude));
+        marker.setMarkerType(MapPOIItem.MarkerType.RedPin);
+        mapView.addPOIItem(marker);
+
+        mapViewContainer.addView(mapView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOff);
+        mapView.setShowCurrentLocationMarker(false);
+    }
+
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+        currentLatitude = mapView.getMapCenterPoint().getMapPointGeoCoord().latitude;
+        currentLongitude = mapView.getMapCenterPoint().getMapPointGeoCoord().longitude;
+        Log.e("current_move", currentLatitude+" / "+currentLongitude);
+    }
+
+    @Override
+    public void onCurrentLocationUpdate(MapView mapView, MapPoint currentLocation, float v) {
+        currentLatitude = currentLocation.getMapPointGeoCoord().latitude;
+        currentLongitude = currentLocation.getMapPointGeoCoord().longitude;
+        Log.e("current", currentLatitude+" / "+currentLongitude);
+    }
+
+    @Override
+    public void onCurrentLocationDeviceHeadingUpdate(MapView mapView, float v) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateFailed(MapView mapView) {
+        Toast.makeText(this, "현재 위치 인식 실패", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCurrentLocationUpdateCancelled(MapView mapView) {
+        Toast.makeText(this, "현재 위치 인식 취소", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
